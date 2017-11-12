afcd42e9527f81102131f5ef134969f22771dab4
kumaser <kustkovmaksim@gmail.com>
2017-11-13 00:54:13 +0400

    presentation auto slide implemented

--- app/src/main/java/ru/boat/view/holders/PresentationViewHolder.java
+++ app/src/main/java/ru/boat/view/holders/PresentationViewHolder.java
@@ -1,10 +1,12 @@
 package ru.boat.view.holders;
 
-
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import me.relex.circleindicator.CircleIndicator;
 import ru.boat.R;
@@ -17,6 +19,8 @@ public class PresentationViewHolder extends RecyclerView.ViewHolder {
     private TextView title;
     private InsideViewPager pager;
     private CircleIndicator indicator;
+    private Timer timer;
+    private Handler handler = new Handler();
 
     public PresentationViewHolder(View itemView) {
         super(itemView);
@@ -30,5 +34,35 @@ public class PresentationViewHolder extends RecyclerView.ViewHolder {
         pager.setId(position + 1);
         pager.setAdapter(new ViewPagerAdapter(presentation.getPictures()));
         indicator.setViewPager(pager);
+        startPresentation();
     }
+
+    private void startPresentation() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timer = new Timer();
+        PresentationTimerTask timerTask = new PresentationTimerTask();
+        timer.schedule(timerTask, 5000);
+    }
+
+    private void showNextSlide() {
+        int nextPosition = pager.getCurrentItem() + 1;
+        int currentItem = nextPosition < 5 ? nextPosition : 0;
+        pager.setCurrentItem(currentItem);
+    }
+
+    class PresentationTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    showNextSlide();
+                    startPresentation();
+                }
+            });
+        }
+    }
+
 }
